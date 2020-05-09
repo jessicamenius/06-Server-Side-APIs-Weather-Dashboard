c26d3b3f5bdd1fb7ec88c8d38502a76f0f666670
Jessica <jessicamenius@gmail.com>
2020-05-08 20:16:52 -0500

    added UV Index

--- app.js
+++ app.js
@@ -1,66 +1,62 @@
 $(document).ready(function () {
+  $("#currentDate").text(moment().format("MMMM DD, YYYY"));
+
   var inputText = "";
   var apiID = "a50d1656c773eed4f6ac56768b7f8ba2";
 
   $("#submitBtn").on("click", function (e) {
     e.preventDefault();
     inputText = $("#inputText").val();
-    $("#inputText").val();
+    renderCity();
 
     $.ajax({
       type: "GET",
       url: `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=${apiID}`,
       dataType: "json",
     }).then(function (res) {
-      console.log(res.name);
+      // console.log(res);
       var cityName = res.name;
+      var windSpeed = res.wind.speed;
       var tempK = res.main.temp;
       var tempF = (tempK - 273.15) * (9 / 5) + 32;
-      var tempF2 = tempF.toFixed(2);
-      var windSpeed = res.main.windspeed;
-      var currentDay = moment().format("MMMM DD, YYYY");
+      var tempF2 = tempF.toFixed(0);
+      var humidity = res.main.humidity;
+      var lat = res.coord.lat;
+      var lon = res.coord.lon;
 
       localStorage.setItem("cityList", JSON.stringify({ city: cityName }));
       renderCity();
 
+      $("#currentCity").append(`<h4>${cityName}</h4>`);
+      $("#currentCity").append(`<br />`);
       $("#currentCity").append(`<p>Temperature: ${tempF2}°F</p>`);
+      $("#currentCity").append(`<p>Humidity: ${humidity}%</p>`);
       $("#currentCity").append(`<p>Wind Speed: ${windSpeed} MPH</p>`);
-      $("#currentDate").append(`<h4>${currentDay}</h4>`);
-      $("#currentCity").prepend(`<h4>${cityName}</h4>`);
 
       $("#cityList").append(`<li class="btn list-group-item">${cityName}</li>`);
+
+      var lat = res.coord.lat;
+      var lon = res.coord.lon;
+
+      $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=a50d1656c773eed4f6ac56768b7f8ba2`,
+        dataTye: "json",
+      }).then(function (uv) {
+        console.log(uv);
+
+        var uvIndex = uv.current.uvi;
+
+        $("#currentCity").append(`<p>UV Index: ${uvIndex} </p>`);
+      });
     });
-    // $(document).on("click", ".btn", function () {
-    //   var text = $(this).text();
-    //   console.log(this);
-
-    // $.ajax({
-    //   type: "GET",
-    //   url: `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${apiID}`,
-    //   dataType: "json",
-    // }).then(function (res) {
-    //   console.log(res.main.temp);
-    //   var cityName = res.name;
-    //   var tempK = res.main.temp;
-    //   var tempF = (tempK - 273.15) * (9 / 5) + 32;
-    //   var tempF2 = tempF.toFixed(2);
-    //   var windSpeed = res.main.windspeed;
-    //   var currentDay = moment().format("MMMM DD, YYYY");
-
-    //   $("#currentCity").append(`<p>Wind Speed: ${windSpeed} MPH</p>`);
-    //   $("#currentCity").append(`<p>Temperature: ${tempF2}°F</p>`);
-    //   $("#currentDate").append(`<h4>${currentDay}</h4>`);
-    //   $("#currentCity").prepend(`<h4>${cityName}</h4>`);
-    // });
-
-    function renderCity() {
-      var item = JSON.parse(localStorage.getItem("cityList"));
-      var currentHour = moment().format("h");
-
-      if (item != null) {
-        $("textInput6").val(item.task);
-      }
-    }
   });
+
+  function renderCity() {
+    var city = JSON.parse(localStorage.getItem("cityList"));
+
+    if (city != null) {
+      $("inputText").val(city.cityName);
+    }
+  }
 });
-// });
